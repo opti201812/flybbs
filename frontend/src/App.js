c6f733dba8d0ec6bcb7eda57bbd567a6e8976b4c
opti201812 <jin_gc@sina.com>
2022-01-17 20:05:39 +0800

    front web initial

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ThreadList from './ThreadList'
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Thread List</h1>
+      <ThreadList />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/jsx/ThreadList.jsx
@@ -0,0 +1,60 @@
+const React, { useState, useEffect } = require('react');
+import {HOST, PORT} from '../config'
+
+const ThreadList = (props) => {
+    //const { threads } = props;
+
+    const [threads, setThreads] = useState([]);
+    useEffect(() => {
+        setThreads([{ title: "slj", author: "john", fav: true }, { title: "slj", author: "john", fav: false }]);
+        const loadThreads = async () => {
+            try {
+                const res = await fetch(`${HOST}:${PORT}/api/threads`, { method: 'GET' });
+                const result = await res.json();
+                if (res.OK) {
+                    setThreads(result.data);
+                } else {
+                    alert(result.message);
+                }
+            } catch (error) {
+                alert(error.message);
+            }
+        };
+        loadThreads();
+    }, [])
+    const rows = threads.map((thread, index) => (
+        <Row key={index} thread={thread} />
+    ));
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Author</th>
+                    <th>Publish</th>
+                    {/* <th>Clicks</th>
+                    <th>Favoriate</th> */}
+                </tr>
+            </thead>
+            <tbody>
+                {rows}
+            </tbody>
+        </table>
+    );
+}
+
+const Row = (props) => {
+    const { thread } = props;
+    const [click, setClick] = useState(0);
+    const [fav, setFav] = useState(thread.fav);
+    return (
+        <tr>
+            <td>{thread.title}</td>
+            <td>{thread.author}</td>
+            <td>{new Date(thread.posttime).toLocaleTimeString()}</td>
+            {/* <td><button onClick={() => setClick(click + 1)}>{click}</button></td>
+            <td><button onClick={() => handleFav(fav ? setFav(false) : setFav(true))}>{fav ? 'Unfav' : 'Fav'}</button></td> */}
+        </tr>
+    )
+}
+export default ThreadList;
