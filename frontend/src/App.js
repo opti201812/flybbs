05681aa9b19b10917075aa00a8e4a1cf392b193a
opti201812 <jin_gc@sina.com>
2022-01-18 21:57:37 +0800

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,17 @@
 import './App.css';
-import ThreadList from './ThreadList'
+import ThreadList from './jsx/ThreadList'
+import RegisterForm from './jsx/RegisterForm'
+import LoginForm from './jsx/LoginForm';
+import SettingForm from './jsx/SettingForm';
+import PostForm from './jsx/PostForm';
+import ModifyButton from './jsx/ModifyButton';
+import ReplyForm from './jsx/ReplyForm';
+import Footer from './jsx/Footer';
+import Header from './jsx/Header';
+import UserInfo from './jsx/UserInfo';
+import Intro from './jsx/Intro';
+import Thread from './jsx/Thread';
+import DeleteButton from './jsx/DeleteButton';
 
 function App() {
   return (
