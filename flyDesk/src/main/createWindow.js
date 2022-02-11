c5870de639ecc3bda9f69fe3dea2e14041a6f5ec
Kevin <tronjin21@gmail.com>
2022-02-11 15:24:15 +0800

    fix import ipcRenderer issue

--- flyDesk/src/main/createWindow.js
+++ flyDesk/src/main/createWindow.js
@@ -6,12 +6,14 @@ const createWindow = () => {
     win = new BrowserWindow({
         width: 800,
         height: 600,
-        WebPreferences: {
+        webPreferences: {
             nodeIntegration: true,
-        },
+            contextIsolation: false,
+         },
     });
     win.loadURL(`file://${__dirname}/../index.html`);
     win.on('closed', () => { win = null });
+    win.webContents.openDevTools();
 };
 
 const getWindow = () => win;
