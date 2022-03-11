e7e65e7d905ae67f2ffaad544d795750f692e114
Kevin <tronjin21@gmail.com>
2022-03-11 14:41:42 +0800

    remove formatpainter according to latest license

--- frontend/src/jsx/ReplyForm.jsx
+++ frontend/src/jsx/ReplyForm.jsx
@@ -47,8 +47,8 @@ const ReplyForm = (props) => {
                     init={{
                         language: 'en',
                         menubar: false,
-                        plugins: 'preview searchreplace autolink directionality visualblocks visualchars fullscreen image link template code codesample table charmap hr pagebreak nonbreaking anchor insertdatetime advlist lists wordcount imagetools textpattern help emoticons autosave autoresize formatpainter',
-                        toolbar: 'code undo redo restoredraft | cut copy paste pastetext | forecolor backcolor bold italic underline strikethrough link anchor | alignleft aligncenter alignright alignjustify outdent indent | styleselect formatselect fontselect fontsizeselect | bullist numlist | blockquote subscript superscript removeformat | table image media charmap emoticons hr pagebreak insertdatetime print preview | fullscreen | bdmap indent2em lineheight formatpainter axupimgs',
+                        plugins: 'preview searchreplace autolink directionality visualblocks visualchars fullscreen image link template code codesample table charmap hr pagebreak nonbreaking anchor insertdatetime advlist lists wordcount imagetools textpattern help emoticons autosave autoresize',
+                        toolbar: 'code undo redo restoredraft | cut copy paste pastetext | forecolor backcolor bold italic underline strikethrough link anchor | alignleft aligncenter alignright alignjustify outdent indent | styleselect formatselect fontselect fontsizeselect | bullist numlist | blockquote subscript superscript removeformat | table image media charmap emoticons hr pagebreak insertdatetime print preview | fullscreen | bdmap indent2em lineheight axupimgs',
                         fontsize_formats: '12px 14px 16px 18px 24px 36px 48px 56px 72px',
                         images_upload_handler: (blobInfo, success, failure) => { 
                             let base64 = "data:image/png;base64," + blobInfo.base64();
