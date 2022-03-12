758886434a71fbf04d370cfb3a7080cbb9bb56da
opti201812 <jin_gc@sina.com>
2022-03-13 01:24:20 +0800

    replaced tinymce with WebView

--- flyMobile/screen/ThreadDetailScreen.js
+++ flyMobile/screen/ThreadDetailScreen.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Alert, View } from "react-native";
-import { Editor } from '@tinymce/tinymce-react';
+// import { Editor } from '@tinymce/tinymce-react';
 import { NativeBaseProvider, Container, Avatar, Text, VStack, Box, Divider, HStack } from 'native-base';
 import { REQ_THREADS_API, REQ_API_ROOT } from '../config.js';
-// import WebView from 'react-native-webview'
+import { WebView } from 'react-native-webview';
 
 const ThreadDetailScreen = (props) => {
     const tid = props.navigation.getParam('thread')._id;
@@ -28,7 +28,7 @@ const ThreadDetailScreen = (props) => {
     const injectedJs = 'setInterval(() => {window.parent.postMessage(document.getElementById("content").clientHeight)}, 500)';
     const ContentCard = (props) => {
         const { data } = props;
-
+        const [htmlHeight, setHtmlHeight] = useState(150);
         return (
             <Box m={2} width="99%">
                 <VStack>
@@ -36,15 +36,15 @@ const ThreadDetailScreen = (props) => {
                         <Avatar size="md" source={{
                             uri: REQ_API_ROOT + (data.author.avatar ? `/upload/${data.author.avatar}` : `/img/avatar.jpeg`),
                         }} />
-                        <VStack space={1} mt={1} width="60%">
+                        <VStack space={1} mt={1} width="45%">
                             <Text fontSize="sm" fontWeight="bold">{data.author.username}</Text>
                             <Text fontSize="xs" maxW="90%">{data.author.description}</Text>
                         </VStack>
-                        <Box width="30%" height="100%" mt={1} alignItems="flex-end">
+                        <Box width="40%" height="100%" mt={1} alignItems="flex-end">
                             <Text fontSize="xs" alignContent="flex-end">{new Date(data.posttime).toLocaleString()}</Text>
                         </Box>
                     </HStack>
-                    <Editor
+                    {/* <Editor
                         initialValue={data.content}
                         id={"tinyEditor"+data._id}
                         apiKey="ptr6mblaq31o1ghf2979iusmzxd367ds7xtdoukeb5r3wbuf"
@@ -58,24 +58,22 @@ const ThreadDetailScreen = (props) => {
                             readonly: true,
                             toolbar: false,
                             readonly: 1,
-                        }} />
-                    {/* <WebView
-                        style={{
-                            width: Dimensions.get('window').width,
-                            height: this.state.height
-                        }}
-                        injectedJavaScript={injectedJs}
-                        automaticallyAdjustContentInsets={true}
-                        source={{ html: `<!DOCTYPE html><html> <style type="text/css"> .tour_product_explain img{ display: block!important; vertical-align: top!important; width: 100%!important;} .tour_product_explain{ padding: 0 15px 20px 15px;} .tour_product_explain *{text-align: left!important; font-size: 14px!important; line-height: 1.3!important; font-family: Arial,"Lucida Grande",Verdana,"Microsoft YaHei",hei!important; float: none!important; padding: 0!important; position: static!important; height: auto!important} </style><body><div class='tour_product_explain' id='content'>${this.state.value}</div></body></html>` }}
-                        scalesPageToFit={true}
-                        javaScriptEnabled={true} // 仅限Android平台。iOS平台JavaScript是默认开启的。
-                        domStorageEnabled={true} // 适用于安卓a
-                        scrollEnabled={false}
-                        onMessage={(event) => {
-                            console.log(event.nativeEvent.data)
-                            this.setState({ height: +event.nativeEvent.data })
-                        }}
-                    /> */}
+                        }} /> */}
+                    <Box width="100%" height={htmlHeight}>
+                        <WebView
+                            injectedJavaScript={injectedJs}
+                            automaticallyAdjustContentInsets={true}
+                            source={{ html: data.content }}
+                            scalesPageToFit={false}
+                            javaScriptEnabled={true}
+                            domStorageEnabled={true}
+                            scrollEnabled={false}
+                            onMessage={(event) => {
+                                console.log(event.nativeEvent.data)
+                                setHtmlHeight(+event.nativeEvent.data);
+                            }}
+                        />
+                    </Box>
                 </VStack>
             </Box>
         )
